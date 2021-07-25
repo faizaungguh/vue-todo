2a82ae3b4c0da28515f41004db7db5c12b0a5fbd
Ungguh Faizaturrohman <otemoos@outlook.com>
2021-07-25 17:10:35 +0700

    watcher

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,21 @@
 <template>
-  <!-- mengembalikan nilai string -->
   <div>
-    {{ num1 + num2 }}
+    {{message}}
   </div>
-  <!-- dikonversi string to Int -->
-  <div>
-    {{ parseInt(num1) + parseInt(num2) }}
-  </div>
-  <!-- menggunakan computed total -->
-  <div>
-    {{ total }}
-  </div>
-  <input v-model="num1" />
-  <!-- menggunakan computed perbandingan -->
-  {{ perbandingan }}
-  <input v-model="num2" />
+  <input v-model="message"/>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      num1: 0,
-      num2: 0,
-    };
+      message: 'test',
+    }
   },
-  computed: {
-    total() {
-      return parseInt(this.num1) + parseInt(this.num2);
-    },
-    perbandingan() {
-      if (this.num1 > this.num2) {
-        return ">";
-      }
-      return "<";
-    },
-  },
-};
+  watch: {
+    message(value){
+      console.log('perubahan di message itu adalah ' + value);
+    }
+  }
+}
 </script>
