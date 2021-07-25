4d5f85c7817c3c1f60d84ed1b57314cd2e4ae596
Ungguh Faizaturrohman <otemoos@outlook.com>
2021-07-25 16:06:13 +0700

    v-once render sekali saja

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,14 @@
 <template>
-  <div id='lifecycle'>{{ message }}</div>
-
-  <button @click="message = 'tulisan setelah dirender ulang'">Update</button>
+  <div v-once>{{ message }}</div>
+  <button @click="message = 'berubah loh'">Update</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: 'text awal',
+      message: 'Hi bro'
     };
-  },
-  // dipanggil sebelum created dijalankan
-  beforeCreate() {
-    console.log('before create');
-  },
-  // dipanggil sebelum template dirender
-  created() {
-    console.log('created');
-    // console.log(message);
-    // console.log(document.getElementById('lifecycle'));
-  },
-  // dijalankan setelah template dirender
-  beforeMount() {
-    console.log('before mount');
-  },
-  // dijalankan setelah template dirender
-  mounted() {
-    console.log('mounted');
-    console.log(message);
-    console.log(document.getElementById('lifecycle'));
-  },
-  // dijalankan ketika ada perubahan atau merender ulang
-  beforeUpdate() {
-    console.log('before update');
-    console.log(document.getElementById('lifecycle').textContent);
-  },
-  // dijalankan ketika ada perubahan atau merender ulang
-  updated() {
-    console.log('updated');
-    console.log(document.getElementById('lifecycle').textContent);
-  },
-  // dijalankan kerika component dihancurkan
-  beforeUnmount() {
-    console.log('before destroy');
-  },
-  // dijalankan kerika component dihancurkan
-  unmounted() {
-    console.log('before destroy');
   },
 };
 </script>
