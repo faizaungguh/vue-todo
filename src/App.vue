fbcd3d111b2b02990e6f0057cbec4427863d4e06
Ungguh Faizaturrohman <otemoos@outlook.com>
2021-07-25 15:55:39 +0700

    lifecycle hook

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,53 @@
 <template>
-  <div>Ini Adalah Parent Component</div>
+  <div id='lifecycle'>{{ message }}</div>
 
-  <child-component
-  v-for="todo in todos"
-  :key="todo"
-  :text="todo"
-  />
+  <button @click="message = 'tulisan setelah dirender ulang'">Update</button>
 </template>
 
 <script>
-import ChildComponent from "./components/ChildComponent.vue";
 export default {
-  components: { ChildComponent },
   data() {
     return {
-      todos:[ 'pertama', 'kedua', 'ketiga']
+      message: 'text awal',
     };
+  },
+  // dipanggil sebelum created dijalankan
+  beforeCreate() {
+    console.log('before create');
+  },
+  // dipanggil sebelum template dirender
+  created() {
+    console.log('created');
+    // console.log(message);
+    // console.log(document.getElementById('lifecycle'));
+  },
+  // dijalankan setelah template dirender
+  beforeMount() {
+    console.log('before mount');
+  },
+  // dijalankan setelah template dirender
+  mounted() {
+    console.log('mounted');
+    console.log(message);
+    console.log(document.getElementById('lifecycle'));
+  },
+  // dijalankan ketika ada perubahan atau merender ulang
+  beforeUpdate() {
+    console.log('before update');
+    console.log(document.getElementById('lifecycle').textContent);
+  },
+  // dijalankan ketika ada perubahan atau merender ulang
+  updated() {
+    console.log('updated');
+    console.log(document.getElementById('lifecycle').textContent);
+  },
+  // dijalankan kerika component dihancurkan
+  beforeUnmount() {
+    console.log('before destroy');
+  },
+  // dijalankan kerika component dihancurkan
+  unmounted() {
+    console.log('before destroy');
   },
 };
 </script>
