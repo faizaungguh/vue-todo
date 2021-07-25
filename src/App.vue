870f261860677a8430f1c2c1c25cbd83546e1972
Ungguh Faizaturrohman <otemoos@outlook.com>
2021-07-25 16:30:58 +0700

    somputed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <template>
-  <div v-html="message">
+  <!-- mengembalikan nilai string -->
+  <div>
+    {{ num1 + num2 }}
   </div>
+  <!-- dikonversi string to Int -->
+  <div>
+    {{ parseInt(num1) + parseInt(num2) }}
+  </div>
+  <!-- menggunakan computed total -->
+  <div>
+    {{ total }}
+  </div>
+  <input v-model="num1" />
+  <!-- menggunakan computed perbandingan -->
+  {{ perbandingan }}
+  <input v-model="num2" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: '<span style ="color: blue">biru</span>'
-    }
+      num1: 0,
+      num2: 0,
+    };
   },
-}
+  computed: {
+    total() {
+      return parseInt(this.num1) + parseInt(this.num2);
+    },
+    perbandingan() {
+      if (this.num1 > this.num2) {
+        return ">";
+      }
+      return "<";
+    },
+  },
+};
 </script>
